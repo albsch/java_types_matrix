23b02e7f4663e5ae45e29f8218d067151b8d4e90
Albert Schimpf <git@schimpf.link>
2020-11-24 11:57:30 +0100

    Less code

--- src/main/java/test/T.java
+++ src/main/java/test/T.java
@@ -2,16 +2,13 @@ package test;
 
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
-import java.util.function.Supplier;
 
 /**
  * Token class which saves the type with generics at runtime.
  */
-public abstract class T<X> implements Supplier<Type> {
+public abstract class T<X> {
 	protected final Type type;
-    public T() { this.type = resolveType(); }
-
-    @Override public Type get() { return type; }
+  public T() { this.type = resolveType(); }
 
 	// this resolves and saves the type with generics at runtime
 	// enables reasoning about generics even with Java type erasure, to an extent
